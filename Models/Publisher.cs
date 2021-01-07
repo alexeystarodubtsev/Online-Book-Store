20689d985dadc3837e4bec1fbd75a38bf34d8df3
aidasark <sarkulova.aidaa2000@gmail.com>
2021-01-07 23:23:13 +0300

    Fix 1

---
+++ Models/Publisher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Online_book_store.Models
+{
+    public class Publisher
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+   
+    }
+}
